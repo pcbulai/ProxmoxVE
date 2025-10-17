2cc71427f064c6cb1b3cd2ca3df543539a5a1b9b
Paul Bulai <paul.bulai@icloud.com>
2025-10-17 11:08:47 +0300

    changed them paths

--- ct/mullvad.sh
+++ ct/mullvad.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/pcbulai/ProxmoxVE/main/misc/build.func)
 
 
 APP="Mullvad-Exit"
